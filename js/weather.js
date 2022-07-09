3b139a25c7420ea076ab80a8a6aa702fbd1d69b0
bingmeng158 <1580777927@qq.com>
2022-07-09 20:23:39 +0800

    Site updated: 2022-07-09 20:23:38

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+WIDGET = {
+    "CONFIG": {
+      "modules": "01234",
+      "background": "1",
+      "tmpColor": "FFFFFF",
+      "tmpSize": "16",
+      "cityColor": "FFFFFF",
+      "citySize": "16",
+      "aqiColor": "FFFFFF",
+      "aqiSize": "16",
+      "weatherIconSize": "24",
+      "alertIconSize": "18",
+      "padding": "10px 10px 10px 10px",
+      "shadow": "0",
+      "language": "auto",
+      "fixed": "false",
+      "vertical": "top",
+      "horizontal": "left",
+      "key": "27042a1cc3ed4bca949b2151c90b5b23"
+    }
+  }
